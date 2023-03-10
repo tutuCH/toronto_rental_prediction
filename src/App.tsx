90fd775e1958737b97fb67e8647146e0324313c5
Harry Tu <tuchenhsien@gmail.com>
2023-03-11 04:24:55 +0800

    init config files

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import type { Component } from 'solid-js';
+import Landing from './pages/Landing/Landing';
+
+const App: Component = () => {
+  return (
+    <div>
+      <Landing/>
+    </div>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx,css,md,mdx,html,json,scss}',
+  ],
+  darkMode: 'class',
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#166534",
+          "secondary": "#F000B8",
+          "accent": "#37CDBE",
+          "neutral": "#3D4451",
+          "base-100": "#FFFFFF",
+          "info": "#3ABFF8",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        },
+      },
+    ],
+  },  
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+};
